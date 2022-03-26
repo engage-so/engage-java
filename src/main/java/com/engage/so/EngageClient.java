63453b5b9a6201e151a6a4c46185648467714d38
franc <onyishif@gmail.com>
2022-03-26 20:31:39 +0100

    bootstrap with maven

---
+++ src/main/java/com/engage/so/EngageClient.java
@@ -0,0 +1,13 @@
+package com.engage.so;
+
+/**
+ * Hello world!
+ *
+ */
+public class EngageClient
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
